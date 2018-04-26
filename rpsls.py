000829f60d2cf8b1454c7118a53e2576de3547b8
SusanRice <34111847+SusanRice@users.noreply.github.com>
2018-04-26 01:25:12 +0000

    Create rpsls.py

---
+++ rpsls.py
@@ -0,0 +1,16 @@
+"""
+Week 4 practice project template for Python Programming Essentials
+Rock-paper-scissors-lizard-Spock
+"""
+
+import random
+
+#The key idea of the program is to equate the strings 
+#"rock", "paper", "scissors, "lizard, "Spock", to numbers as follows:
+
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - Scissors
+
